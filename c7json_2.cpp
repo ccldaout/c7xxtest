f500784238fbc7e389829e3aa6a3e4763a5cb54c
Masakazu Iwanaga <ccldaout@gmail.com>
2025-05-15 05:55:48 +0900

    json_basic<T>::print_as

--- c7json_2.cpp
+++ c7json_2.cpp
@@ -12,11 +12,13 @@ struct Song: public c7::json_object {
     c7::json_str	title;
     c7::json_int	duration_s;
     c7::json_bin	audio;
+    c7::json_bool	favorite;
 
     Song() {
 	c7json_member(title);
 	c7json_member(duration_s);
 	c7json_member(audio);
+	c7json_member(favorite);
     }
 };
 
@@ -57,6 +59,7 @@ static void init_lib(Library& lib, const std::string& path)
 	    s.title = "Hungry Daze";
  	    "hungry daze" | c7::nseq::push_back(s.audio());
 	    s.duration_s = 501;
+	    s.favorite   = false;
 	}
 	lib.albums.push_back(std::move(a));
     }
@@ -71,6 +74,7 @@ static void init_lib(Library& lib, const std::string& path)
 	    s.title = "Heat of the moment";
 	    s.duration_s = 228;
  	    "heat of the moment" | c7::nseq::push_back(s.audio());
+	    s.favorite   = true;
 	    a.songs.push_back(std::move(s));
 	}
     }
@@ -105,12 +109,12 @@ int main(int argc, char **argv)
 	if (a.title() != album) {
 	    continue;
 	}
-	p_("Hit! album: title:%{}, artist:%{}, release:%{t%Y}", album, a.artist(), a.release());
+	p_("Hit! album: title:%{}, artist:%{}, release:%{t%Y}", album, a.artist, a.release);
 	for (auto& s: a.songs) {
 	    if (s.title() != song) {
 		continue;
 	    }
-	    p_("Hit! song: title:%{}, duration_s:%{}", s.title(), s.duration_s());
+	    p_("Hit! song: title:%{}, duration_s:%{}", s.title, s.duration_s);
 	    auto& d = s.audio();
 	    for (auto c: data) {
 		d.push_back(c);
